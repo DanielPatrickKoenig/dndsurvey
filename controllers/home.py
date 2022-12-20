b902c946fccab02aac9471d3f9ee82dfa49e2f2e
dan koenig <dangerbird@dans-MBP.attlocal.net>
2022-12-19 21:16:14 -0500

    data services created

--- controllers/home.py
+++ controllers/home.py
@@ -3,7 +3,6 @@ from os.path import exists
 def get_base_path(req):
     return 'applications/'+req.application
 
-@auth.requires_login()
 def index():
     dir = get_base_path(request) + '/static/js/home/build/static'
     jsItems = os.listdir(dir+'/js')
@@ -13,7 +12,8 @@ def index():
     return dict(
         message=T('you are home'),
         cssFile=cssFiles[0],
-        jsFile=jsFiles[0]
+        jsFile=jsFiles[0],
+        appName=request.application
     )
 
 def get_file_path(req):
@@ -21,16 +21,16 @@ def get_file_path(req):
     return get_base_path(req) + '/static/state_' + str(user_id) + '.txt'
 
 def default_user_content():
-    return ''
+    return 'empty'
 
 @auth.requires_login()
 def load_data():
     user_id = auth.user.id
     if exists(get_file_path(request)):
         f = open(get_file_path(request), "r")
-        return response.json(dict(content=f.read()))
+        return response.json(dict(content=f.read(), status='success'))
     else:
-        return response.json(dict(content=default_user_content()))
+        return response.json(dict(content=default_user_content(), status='success'))
 
 @auth.requires_login()
 def save_data():
@@ -38,10 +38,9 @@ def save_data():
     f = open(get_file_path(request), "w")
     f.write(data_to_save)
     f.close()
-
-
-def get_questions():
-    types = {
+    return response.json(dict(status='success'))
+def get_types():
+    return {
         'paladin': 1,
         'druid': 2,
         'wizard': 3,
@@ -55,6 +54,8 @@ def get_questions():
         'sorcerer': 11,
         'warlock': 12
     }
+def get_questions():
+    types = get_types()
     questions = [
         {
             "text": "The most important part of a battle is looting the bodies of your enemies.",
@@ -298,4 +299,26 @@ def get_questions():
         }
     ]
     return response.json(dict(types=types, questions=questions))
+def process_results():
+    data_to_process = request.vars['content']
+    section_splitter = request.vars['section_splitter']
+    item_splitter = request.vars['item_splitter']
+    value_splitter = request.vars['value_splitter']
+    sections = data_to_process.split(section_splitter)
+    scores = sections[0].split(item_splitter)
+    score_matrix = [{'value': s.split(value_splitter)[0], 'code': s.split(value_splitter)[1]} for s in scores]
+    types = get_types()
+    type_names = list(types.keys())
+    # print([types[t] for t in type_names])
+    # print([s['value'] for s in score_matrix])
+    processed_scores = {n: 0 for n in type_names}
+    for s in score_matrix:
+        for t in type_names:
+            if int(s['code']) == types[t]:
+                processed_scores[t] = int(processed_scores[t]) + int(s['value'])
+    sorted_values = [processed_scores[n] for n in type_names]
+    sorted_values.sort()
+    lowest_value = sorted_values[0]
+    return response.json([n for n in type_names if processed_scores[n] == lowest_value])
+    
     
