df12ed57bfa345cd857d215a98f0716eea91d0bc
Vicky Lu <vl@Vickys-Laptop.attlocal.net>
2023-02-04 11:02:10 -0500

    code so far

--- static/js/home/src/App.js
+++ static/js/home/src/App.js
@@ -1,22 +1,11 @@
-import './App.css';
+import TopNav from './components/TopNav';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="/dndsurvey/static/images/logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopNav />
     </div>
   );
 }
---
+++ static/js/home/src/components/TopNav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../../src/App.scss';
+
+
+const TopNav = () => {
+    return(
+        <div className='top-nav-container'>
+            <ul>
+                <li>Home</li>
+                <li>Instructions</li>
+            </ul>
+        </div>
+    );
+}
+
+export default TopNav;
