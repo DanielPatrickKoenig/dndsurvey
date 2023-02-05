95cfb3d9ea7e06c8fc925c31f0df80f18e8ade6f
Vicky Lu <vl@Vickys-Laptop.attlocal.net>
2023-02-05 18:40:19 -0500

    create new branch for top nav

--- static/js/home/src/components/TopNav.js
+++ static/js/home/src/components/TopNav.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import LogoSVG from './LogoSVG';
+
 import '../../src/App.scss';
 
 
 const TopNav = () => {
     return(
         <div className='top-nav-container'>
+            <LogoSVG />
             <ul>
                 <li>Home</li>
                 <li>Instructions</li>
