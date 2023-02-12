96ff3edb852f1cf15eec693017eadacd07d18596
Vicky Lu <vl@Vickys-Laptop.attlocal.net>
2023-02-11 19:07:51 -0500

    1st complete attempt

--- static/js/home/src/App.js
+++ static/js/home/src/App.js
@@ -1,12 +1,10 @@
 import TopNav from './components/TopNav';
-import HamburgerMenu from './components/HamburgerMenu';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <HamburgerMenu />
       <TopNav />
     </div>
   );
--- static/js/home/src/components/TopNav.js
+++ static/js/home/src/components/TopNav.js
@@ -1,24 +1,43 @@
-//--- import React related features ---//
-import React from 'react';
-import HamburgerIcon from './HamburgerIcon';
+//--- import React related features
+import React, { useState } from 'react';
 
-//--- import assets created by us ---//
+//--- import Hamburger Icon
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+//--- import assets
 import LogoSVG from './LogoSVG';
 
-//--- import stylesheets ---//
+//--- import stylesheets
 import '../../src/App.scss';
 
 
 const TopNav = () => {
+
+    const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false);
+
+    const hamburgerMenuToggleHandler = () => setHamburgerMenuOpen(!hamburgerMenuOpen);
+
     return (
-        <div className='top-nav-container'>
-            <LogoSVG />
-            <ul>
-                <li>Home</li>
-                <li>Instructions</li>
-            </ul>
-            <div className='hamburger-icon-component'>
-                <HamburgerIcon />
+        <div>
+            <div className='top-nav-container'>
+                <LogoSVG />
+                <ul>
+                    <li>Home</li>
+                    <li>Instructions</li>
+                </ul>
+                <div className='hamburger-icon-container' onClick={hamburgerMenuToggleHandler}>             
+                    { hamburgerMenuOpen ? <FontAwesomeIcon icon={faXmark} className='icon-dark'></FontAwesomeIcon> : <FontAwesomeIcon icon={faBars} className='icon'></FontAwesomeIcon> }
+                </div>
+            </div>
+            <div className={hamburgerMenuOpen ? 'hamburger-menu-container active' : 'hamburger-menu-container inactive' } >
+            {/* <div className='hamburger-menu-container' > */}
+                {/* <LogoSVG /> */}
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Instructions</a></li>
+                </ul>
             </div>
         </div>
     );
